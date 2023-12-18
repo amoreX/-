51494566610e0646defb501097918818c219ecbc
amor3x <71655709+amoreX@users.noreply.github.com>
2023-12-18 23:14:45 +0530

    modified by asad

--- ind.js
+++ ind.js
@@ -35,7 +35,20 @@ function prog22(){
 }
 
 function prog33(){
-    const prog3 ="meow meow"
+    const prog3 ="import numpy as np \n"+
+    "from scipy . integrate import odeint \n"+
+    "import matplotlib . pyplot as plt \n"+
+    "def model (y , t ): \n"+
+    "  k=0.3 \n"+
+    "  return -k*y \n"+
+    "y0=5 \n"+
+    "t=np . linspace (0 , 20 ) \n"+
+    "y= odeint ( model , y0 , t ) \n"+
+    "plt . plot (t , y ) \n"+
+    "plt . title ('Solution of dy/dt= -ky; k=0.3, y(0)=5') \n"+
+    "plt . xlabel ('time ') \n"+
+    "plt . ylabel ('y(t)') \n"+
+    "plt . show () \n"
                  
     navigator.clipboard.writeText(prog3);
 }
@@ -59,6 +72,30 @@ function prog44(){
 }
 
 function prog55(){
-    const prog5 =" meow meow "
+    const prog5 ="f1 = lambda x ,y , z: ( 17-y+2*z )/20 \n"+
+    "f2 = lambda x ,y , z: (-18-3*x+z )/20 \n"+
+    "f3 = lambda x ,y , z: ( 25-2*x+3*y )/20 \n"+
+    "x0 = 0 \n"+
+    "y0 = 0 \n"+
+    "z0 = 0  \n"+
+    "count = 1 \n"+
+    "e = float ( input ('Enter tolerable error : ') ) \n"+
+    "print ('\nCount \tx\ty\tz\n') \n"+
+    "condition = True \n"+
+    "while condition : \n"+
+    "  x1 = f1 ( x0 , y0 , z0 ) \n"+ 
+    "  y1 = f2 ( x1 , y0 , z0 ) \n"+
+    "  z1 = f3 ( x1 , y1 , z0 ) \n"+
+    "  print ('%d\t%0.4f\t%0.4f\t%0.4f\n' %( count , x1 , y1 , z1 ) ) \n"+
+    "  e1 = abs ( x0-x1 ) ;  \n"+
+    "  e2 = abs ( y0-y1 ) ; \n"+
+    "  e3 = abs ( z0-z1 ) ; \n"+
+    "  count += 1 \n"+
+    "  x0 = x1 \n"+
+    "  y0 = y1 \n"+
+    "  z0 = z1 \n"+
+    "  condition = e1>e and e2>e and e3>e \n"+
+    "print ('\nSolution : x=%0.3f , y=%0.3f and z = %0.3f\n'% ( x1 , y1 , z1 ) )"
+
     navigator.clipboard.writeText(prog5);
 }
